0c194574a71965f2f33a3c361b64b757a4d13cfd
Dino Fejzagic <dino.f@live.de>
2020-03-22 12:47:18 +0100

    Add existing code

---
+++ mite-client.ts
@@ -0,0 +1,302 @@
+import { RestClient } from "typed-rest-client/RestClient";
+import { IHeaders } from "typed-rest-client/Interfaces";
+import {
+    MiteUser, MiteUserResponse, MiteAccount, MiteAccountResponse,
+    MiteTracker, MiteTrackerResponse, MiteTimeEntry, MiteTimeEntryResponse, MiteCreateTimeEntryArgs, MiteProject, MiteService, MiteUngroupedTimeEntry
+} from "./mite-models";
+
+export class MiteClient {
+
+    private readonly accountName: string;
+    private readonly apiKey: string;
+    private readonly client: RestClient;
+
+    private readonly getMyselfEndpoint = 'myself.json';
+    private readonly getAccountEndpoint = 'account.json';
+    private readonly getCurrentTrackerEndpoint = 'tracker.json';
+    private readonly getTrackerEndpoint = 'tracker/';
+    private readonly getDailyEndpoint = 'daily.json';
+    private readonly createTimeEntryEndpoint = 'time_entries.json';
+    private readonly getTimeEntriesEndpoint = 'time_entries/'
+    private readonly getActiveProjectsEndpoint = 'projects.json';
+    private readonly getActiveServicesEndpoint = 'services.json';
+
+    /**
+     * Creates a new instance of the mite API client.
+     * @param accountName The mite account name to connect to.
+     * @param apiKey The user's mite API key.
+     */
+    constructor(accountName: string, apiKey: string) {
+        this.accountName = accountName;
+        this.apiKey = apiKey;
+        this.client = new RestClient('mite.azure.devops.extension', 'https://corsapi.mite.yo.lk/');
+    }
+
+    /**
+     * Checks whether the client is authorized to make
+     * requests to the mite API.
+     * 
+     * @returns Promise resolving to authorization state.
+     */
+    public async isAuthorized(): Promise<boolean> {
+        const myself = await this.getMyself();
+        return myself !== null;
+    }
+
+    //#region Account / My User Endpoints
+
+    /**
+     * Gets the currently authenticated user's data.
+     * @returns User data or null if not found / in case of error.
+     */
+    public async getMyself(): Promise<MiteUser | null> {
+        let headers: IHeaders = {};
+        headers['X-MiteAccount'] = this.accountName;
+        headers['X-MiteApiKey'] = this.apiKey;
+
+        const response = await this.client.get<MiteUserResponse>(`${this.getMyselfEndpoint}`, {
+            additionalHeaders: headers
+        });
+
+        if (response.statusCode === 200) {
+            return response.result!.user;
+        }
+
+        return null;
+    }
+
+    /**
+     * Gets the currently authenticated user's account information.
+     * @returns Account data or null if not found / in case of error.
+     */
+    public async getAccount(): Promise<MiteAccount | null> {
+        let headers: IHeaders = {};
+        headers['X-MiteAccount'] = this.accountName;
+        headers['X-MiteApiKey'] = this.apiKey;
+
+        const response = await this.client.get<MiteAccountResponse>(`${this.getAccountEndpoint}`, {
+            additionalHeaders: headers
+        });
+
+        if (response.statusCode === 200) {
+            return response.result!.account;
+        }
+
+        return null;
+    }
+
+    //#endregion Account / My User Endpoints
+
+    //#region Tracker Endpoints
+
+    /**
+     * Gets the currently active tracker.
+     * @returns Tracker data or null, if no active tracker.
+     */
+    public async getTracker(): Promise<MiteTracker | null> {
+        let headers: IHeaders = {};
+        headers['X-MiteAccount'] = this.accountName;
+        headers['X-MiteApiKey'] = this.apiKey;
+
+        const response = await this.client.get<MiteTrackerResponse>(`${this.getCurrentTrackerEndpoint}`, {
+            additionalHeaders: headers
+        });
+
+        if (response.statusCode === 200 && response.result!.tracker !== undefined) {
+            return response.result!.tracker!;
+        }
+
+        return null;
+    }
+
+    /**
+     * Starts the time tracker on a time entry.
+     * @param id The ID of the time entry to start tracking.
+     * @returns Tracker and tracking time entry data.
+     */
+    public async startTracker(id: number): Promise<MiteTracker | null> {
+        let headers: IHeaders = {};
+        headers['X-MiteAccount'] = this.accountName;
+        headers['X-MiteApiKey'] = this.apiKey;
+
+        const response = await this.client.update<MiteTrackerResponse>(`${this.getTrackerEndpoint}${id}.json`, {
+            additionalHeaders: headers
+        });
+
+        if (response.statusCode === 200 && response.result!.tracker !== undefined) {
+            return response.result!.tracker!;
+        }
+
+        return null;
+    }
+
+    /**
+     * Stops the time tracker on a given time entry.
+     * @param id The ID of the time entry to stop tracking.
+     * @returns Data of stopped tracker and time entry.
+     */
+    public async stopTracker(id: number): Promise<MiteTracker | null> {
+        let headers: IHeaders = {};
+        headers['X-MiteAccount'] = this.accountName;
+        headers['X-MiteApiKey'] = this.apiKey;
+
+        const response = await this.client.del<MiteTrackerResponse>(`${this.getTrackerEndpoint}${id}.json`, {
+            additionalHeaders: headers
+        });
+
+        if (response.statusCode === 200 && response.result!.tracker !== undefined) {
+            return response.result!.tracker!;
+        }
+
+        return null;
+    }
+
+    //#endregion Tracker Endpoints
+
+    //#region Time Entries Endpoints
+
+    /**
+     * Gets all existing time entries for the current day.
+     * @returns Time entries, if any.
+     */
+    public async getTimeEntriesForToday(): Promise<MiteTimeEntry[]> {
+        let headers: IHeaders = {};
+        headers['X-MiteAccount'] = this.accountName;
+        headers['X-MiteApiKey'] = this.apiKey;
+
+        const response = await this.client.get<MiteUngroupedTimeEntry[]>(`${this.getDailyEndpoint}`, {
+            additionalHeaders: headers
+        });
+
+        if (response.statusCode === 200) {
+            return response.result!.map(r => r.time_entry);
+        }
+
+        return [];
+    }
+
+    /**
+     * Gets an existing time entrz for a given work item in today's time entries.
+     * @param workItemId The work item ID to find the time entry for in today's entries.
+     */
+    public async getTimeEntryForTodayForWorkItem(workItemId: number): Promise<MiteTimeEntry | null> {
+        const todaysEntries = await this.getTimeEntriesForToday();
+        if (todaysEntries.length > 0) {
+            const index = todaysEntries.findIndex(e => e.note.includes(`${workItemId}###`));
+            if (index > -1) {
+                return todaysEntries[index];
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Gets the currently tracking time entry, if any.
+     * @returns Currently tracking time entry, if any. Otherwise null.
+     */
+    public async getTrackingTimeEntry(): Promise<MiteTimeEntry | null> {
+        const tracker = await this.getTracker();
+        if (tracker && tracker.tracking_time_entry) {
+            const timeEntry = await this.getTimeEntry(tracker.tracking_time_entry.id);
+
+            if (timeEntry !== null) {
+                return timeEntry;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Gets a single time entry by ID.
+     * @param id The ID of the time entry to lookup.
+     * @returns Time entry data, if found. Otherwise null.
+     */
+    public async getTimeEntry(id: number): Promise<MiteTimeEntry | null> {
+        let headers: IHeaders = {};
+        headers['X-MiteAccount'] = this.accountName;
+        headers['X-MiteApiKey'] = this.apiKey;
+
+        const response = await this.client.get<MiteTimeEntryResponse>(`${this.getTimeEntriesEndpoint}${id}.json`, {
+            additionalHeaders: headers
+        });
+
+        if (response.statusCode === 200) {
+            return response.result!.time_entry;
+        }
+
+        return null;
+    }
+
+    /**
+     * Creates a new time entry.
+     * @param args Arguments passed to mite for the time entry created.
+     * @returns Created time entry data.
+     */
+    public async createTimeEntry(args: MiteCreateTimeEntryArgs): Promise<MiteTimeEntry | null> {
+        let headers: IHeaders = {};
+        headers['X-MiteAccount'] = this.accountName;
+        headers['X-MiteApiKey'] = this.apiKey;
+
+        const response = await this.client.create<MiteTimeEntryResponse>(this.createTimeEntryEndpoint, args, {
+            additionalHeaders: headers
+        });
+
+        if (response.statusCode === 201) {
+            return response.result!.time_entry;
+        }
+
+        return null;
+    }
+
+    //#endregion Time Entries Endpoints
+
+    //#region Projects Endpoints
+
+    /**
+     * Gets all active mite projects for the current user.
+     * @returns List of mite projects found.
+     */
+    public async getActiveProjects(): Promise<MiteProject[]> {
+        let headers: IHeaders = {};
+        headers['X-MiteAccount'] = this.accountName;
+        headers['X-MiteApiKey'] = this.apiKey;
+
+        const response = await this.client.get<MiteProject[]>(`${this.getActiveProjectsEndpoint}`, {
+            additionalHeaders: headers
+        });
+
+        if (response.statusCode === 200) {
+            return response.result!;
+        }
+
+        return [];
+    }
+
+    //#endregion Projects Endpoints
+
+    //#region Services Endpoints
+
+    /**
+     * Gets all active mite services for the current user.
+     * @returns List of mite services found.
+     */
+    public async getActiveServices(): Promise<MiteService[]> {
+        let headers: IHeaders = {};
+        headers['X-MiteAccount'] = this.accountName;
+        headers['X-MiteApiKey'] = this.apiKey;
+
+        const response = await this.client.get<MiteService[]>(`${this.getActiveServicesEndpoint}`, {
+            additionalHeaders: headers
+        });
+
+        if (response.statusCode === 200) {
+            return response.result!;
+        }
+
+        return [];
+    }
+
+    //#endregion Services Endpoints
+}
---
+++ mite-models.ts
@@ -0,0 +1,111 @@
+export interface MiteUngroupedTimeEntry {
+    time_entry: MiteTimeEntry;
+}
+
+export interface MiteCreateTimeEntryArgs {
+    time_entry: MiteCreateTimeEntryPayload;
+}
+
+export interface MiteCreateTimeEntryPayload {
+    date_at?: Date;
+    minutes?: number;
+    note?: string;
+    user_id?: number;
+    project_id?: number;
+    service_id?: number;
+    locked?: boolean;
+}
+
+export interface MiteAccountResponse {
+    account: MiteAccount;
+}
+
+export interface MiteAccount {
+    id: number;
+    name: string;
+    title: string;
+    currency: string;
+    created_at: Date;
+    updated_at: Date;
+}
+
+export interface MiteUserResponse {
+    user: MiteUser;
+}
+
+export interface MiteUser {
+    id: number;
+    name: string;
+    email: string;
+    note: string;
+    archived: boolean;
+    role: string;
+    language: string;
+    created_at: Date;
+    updated_at: Date;
+}
+
+export interface MiteTrackerResponse {
+    tracker?: MiteTracker;
+}
+
+export interface MiteTracker {
+    tracking_time_entry: MiteTrackingTimeEntry;
+}
+
+export interface MiteTrackingTimeEntry {
+    id: number;
+    minutes: number;
+    since: Date;
+}
+
+export interface MiteTimeEntryResponse {
+    time_entry: MiteTimeEntry;
+}
+
+export interface MiteTimeEntry {
+    id: number;
+    minutes: number;
+    date_at: Date;
+    note: string;
+    billable: boolean;
+    locked: boolean;
+    revenue: any;
+    hourly_rate: number;
+    user_id: number;
+    user_name: string;
+    project_id: number;
+    project_name: string;
+    customer_id: number;
+    customer_name: string;
+    service_id: number;
+    service_name: string;
+    created_at: Date;
+    updated_at: Date;
+}
+
+export interface MiteProject {
+    id: number;
+    name: string;
+    note: string;
+    customer_id: number;
+    customer_name: string;
+    budget: number;
+    budget_type: string;
+    hourly_rate: number;
+    archived: boolean;
+    active_hourly_rate: string;
+    created_at: Date;
+    updated_at: Date;
+}
+
+export interface MiteService {
+    id: number;
+    name: string;
+    note: string;
+    hourly_rate: number;
+    archived: boolean;
+    billable: boolean;
+    created_at: Date;
+    updated_at: Date;
+}
